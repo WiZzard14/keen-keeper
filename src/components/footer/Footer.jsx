eacb258947a4f3236de65da59446258b47ab2da4
WiZzard <you@example.com>
2026-04-16 22:35:58 +0600

    fixing footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from '../assets/logo-xl.png';
-import facebook from '../assets/facebook.png';
-import instagram from '../assets/instagram.png';
-import twitter from '../assets/twitter.png';
+import logo from '../../assets/logo-xl.png';
+import facebook from '../../assets/facebook.png';
+import instagram from '../../assets/instagram.png';
+import twitter from '../../assets/twitter.png';
 
 const Footer = () => {
     return (
